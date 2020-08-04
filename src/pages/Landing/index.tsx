7b00572f1334bb7ce8d660431032947fdfac874a
Gabriel Santos <gabriel_santos@whirlpool.com>
2020-08-03 22:55:05 -0300

    Add TeacherForm and TeacherList pages and links

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import logoImg from '../../assets/images/logo.svg'
 import landingImg from '../../assets/images/landing.svg'
 import studyIcon from '../../assets/images/icons/study.svg'
@@ -15,21 +16,18 @@ function Landing() {
                     <img src={logoImg} alt="Proffy"/>
                     <h2>Sua plataforma de estudos online</h2>
                 </div>
-                <img 
-                    src={landingImg} 
-                    alt="Plataforma de estudos" 
-                    className="hero-image"
-                />
+                
+                <img src={landingImg} alt="Plataforma de estudos" className="hero-image" />
 
                 <div className="buttons-container">
-                    <a href="" className ="study">
+                    <Link to="/study" className ="study">
                         <img src={studyIcon} alt="Estudar"/>
                         Estudar
-                    </a>
-                    <a href="" className ="give-classes">
+                    </Link>
+                    <Link to="/give-classes" className ="give-classes">
                         <img src={giveClassesIcon} alt="Dar aulas"/>
                         Dar aulas
-                    </a>
+                    </Link>
                 </div>
 
                 <span className="total-connections">
